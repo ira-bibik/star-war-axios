da747170a1039a5c2f4f5ecce69e8457130a3ca0
Iryna Bibik <bibik.iryna004@gmail.com>
2023-03-18 23:17:14 +0200

    Changing styles, adding loading on people page

--- src/components/components/People/People.jsx
+++ src/components/components/People/People.jsx
@@ -1,39 +1,45 @@
 import React, { useEffect, useState } from "react";
 import "./People.css";
 import { Buttons } from "../shared/Buttons";
-import {api, getPerson} from '../../../services/sw-service'
-
+import { api, getPerson } from "../../../services/sw-service";
 
 const People = () => {
-    const [person, setPerson] = useState({});
-    const [currentId, setId] = useState(1);
+  const [person, setPerson] = useState({});
+  const [currentId, setId] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        api.get(`/people/${currentId}/`).then(res => {
-            setPerson(res.data);
-        })
-        // setPerson(getPerson(currentId).data);
-        // console.log(person)
-    },[currentId])
-    
-    return (
-      <>
-        {person ? (
-          <div className="card">
-            <div className="image"></div>
-            <h3>{person.name}</h3>
-            <ul className="list">
-              <li className="list-item">Gender: {person.gender} </li>
-              <li className="list-item">Birth Year: {person.birth_year} </li>
-              <li className="list-item">Eye color: {person.eye_color} </li>
-            </ul>
-            <Buttons id={currentId} changeSmth={setId} />
-          </div>
-        ) : (
-          <h1>No data!</h1>
-        )}
-      </>
-    );
+  useEffect(() => {
+    setIsLoading(true);
+    api.get(`/people/${currentId}/`).then((res) => {
+        setPerson(res.data);
+        setIsLoading(false);
+    });
+    // setPerson(getPerson(currentId).data);
+    // console.log(person)
+  }, [currentId]);
+
+  return (
+    <>
+      {isLoading ? (
+        <p className="load">Loading...</p>
+      ) : (
+        <div className="card">
+          <img
+            className="image"
+            src={`https://starwars-visualguide.com/assets/img/characters/${currentId}.jpg`}
+            alt={person.name}
+          />
+          <h3 className="person-name">{person.name}</h3>
+          <ul className="list">
+            <li className="list-item">Gender: {person.gender} </li>
+            <li className="list-item">Birth Year: {person.birth_year} </li>
+            <li className="list-item">Eye color: {person.eye_color} </li>
+          </ul>
+          <Buttons id={currentId} changeSmth={setId} />
+        </div>
+      )}
+    </>
+  );
 };
 
 export { People };
--- src/components/components/shared/Buttons.jsx
+++ src/components/components/shared/Buttons.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { Button } from "@mui/material";
+import { Button, ButtonGroup } from "@mui/material";
+
+const styles = {
+  margin: "auto"
+}
 
 const Buttons = ({id, changeSmth}) => {
     return (
-      <>
+      <ButtonGroup sx={{ display: "flex", justifyContent: "center" }}>
         <Button
           disabled={id === 1}
           onClick={() => changeSmth((prev) => prev - 1)}
         >
           Prev
         </Button>
-        <Button
-          onClick={() => changeSmth((prev) => prev + 1)}
-        >
-          Next
-        </Button>
-      </>
+        <Button onClick={() => changeSmth((prev) => prev + 1)}>Next</Button>
+      </ButtonGroup>
     );
 }
 
